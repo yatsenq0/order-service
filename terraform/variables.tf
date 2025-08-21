7d9dc8d73c2884ce08a33ae9d52b7cc6af59fe92
yatsenq0 <yatsenq0@vk.com>
2025-08-21 19:21:17 +0300

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
